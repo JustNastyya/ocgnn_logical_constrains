fb027577526ea831978f3fbed0a5a521ea60497f
JustNastyya <druzhkoshowewed@gmail.com>
2026-04-03 12:01:22 +0200

    add: pipeline

--- experiments/node_level/run_bunch_experiments.py
+++ experiments/node_level/run_bunch_experiments.py
@@ -10,6 +10,7 @@ from experiments.node_level.model_reference_nl import model_reference
 from experiments.node_level.run_experiment_nl import experiment_logging_wrapper
 
 from constrains.nl_rules_based_handler import NLRuleBasedHandler
+from constrains.constrains_score_handler import NLConstraintScoreBasedHandler
 
 FILEPATH = "experiments/node_level/bunch_json_results/"
 TRAIN_NORMAL = True
@@ -44,10 +45,11 @@ def experiment_wrapper(
                 "model": default_config["model_train"]["model"].__name__,
                 "train_loop": default_config["model_train"]["train_loop"].__name__,
             },
-            "constrains_handler": default_config["constrains_handler"].__name__,
         },
         "model_results": model_results
     }
+    if default_config["is_logical"]:
+        results["model_config"]["constrains_handler"] = default_config["constrains_handler"].__name__
     
     return results
 
@@ -95,38 +97,37 @@ def run_bunch_experiments(
 
 if __name__ == "__main__":
     default_config = {
-        "hidden_dim": 64,
-        "num_layers": 3,
         "device": "cuda" if torch.cuda.is_available() else "cpu",
         "lr": 1e-3,
         "epochs": 50,
         "batch_size": 32,
         "dataset": "Cora",
-        "model_train": model_reference["logic_add_nl_ocgin"],
-        "is_logical": True,
-        "constrains_filepath": "constrains/data/Cora_auto_generated_2_101_102_103.json",
-        "constrains_handler": NLRuleBasedHandler,
-        "l_factor": 0.1,
         "save_logs": False,
     }
     
     baseline_var_pars = {
-        "hidden_dim": [32],
+        "hidden_dim": [4, 8, 16, 32, 64, 128, 256, 512],
         "num_layers": [2, 3, 4, 5]
     }
     const_var_pars = {
-        "l_factor": [0.1, 0.5, 0.01, 0.001],
-        "constrains_filepath": [
-            "constrains/data/Cora_auto_generated_2_101_102_103.json",
-            "constrains/data/Cora_auto_generated_3_101_102_103.json"
-        ],
+        "l_factor": [1, 0.1, 0.5, 0.01, 0.001],
+        "decision_tree": [{
+            "attribute_list": ["node_features", "node_degree", "clustering_coefficient"],
+            "max_depth": 3,
+        },
+        {
+            "attribute_list": ["node_features", "node_degree", "clustering_coefficient"],
+            "max_depth": 2,
+        }],
         "model_train": [
             model_reference["logic_add_nl_ocgin"],
             model_reference["logic_weight_nl_ocgin"],
             model_reference["logic_ignore_sus_nl_ocgin"]
-        ]
+        ],
+        "constrains_handler": [NLRuleBasedHandler, NLConstraintScoreBasedHandler]
     }
     baseline_model="simple_node_ocgin"
+    
     run_bunch_experiments(
         default_config, 
         baseline_var_pars, 
--- experiments/the_pipeline.py
+++ experiments/the_pipeline.py
@@ -3,52 +3,58 @@ from loguru import logger
 import torch
 from experiments.node_level.model_reference_nl import model_reference
 from experiments.node_level.run_bunch_experiments import run_bunch_experiments
+from experiments.node_level.data_loader_nl import DATASET_REFERENCE
 
 from constrains.nl_rules_based_handler import NLRuleBasedHandler
-
+from constrains.constrains_score_handler import NLConstraintScoreBasedHandler
 
 def the_pipeline():
     
     default_config = {
-        "hidden_dim": 64,
-        "num_layers": 3,
         "device": "cuda" if torch.cuda.is_available() else "cpu",
         "lr": 1e-3,
         "epochs": 50,
         "batch_size": 32,
         "dataset": "Cora",
-        "model_train": model_reference["logic_add_nl_ocgin"],
-        "is_logical": True,
-        "constrains_filepath": "constrains/data/Cora_auto_generated_2_101_102_103.json",
-        "constrains_handler": NLRuleBasedHandler,
-        "l_factor": 0.1,
         "save_logs": False,
     }
     
     baseline_var_pars = {
-        "hidden_dim": [32],
+        "hidden_dim": [4, 8, 16, 32, 64, 128, 256, 512],
         "num_layers": [2, 3, 4, 5]
     }
     const_var_pars = {
-        "l_factor": [0.1, 0.5, 0.01, 0.001],
-        "constrains_filepath": [
-            "constrains/data/Cora_auto_generated_2_101_102_103.json",
-            "constrains/data/Cora_auto_generated_3_101_102_103.json"
-        ],
+        "l_factor": [1, 0.1, 0.5, 0.01, 0.001],
+        "decision_tree": [{
+            "attribute_list": ["node_features", "node_degree", "clustering_coefficient"],
+            "max_depth": 3,
+        },
+        {
+            "attribute_list": ["node_features", "node_degree", "clustering_coefficient"],
+            "max_depth": 2,
+        }],
         "model_train": [
             model_reference["logic_add_nl_ocgin"],
             model_reference["logic_weight_nl_ocgin"],
             model_reference["logic_ignore_sus_nl_ocgin"]
-        ]
+        ],
+        "constrains_handler": [NLRuleBasedHandler, NLConstraintScoreBasedHandler]
     }
     baseline_model="simple_node_ocgin"
-    run_bunch_experiments(
-        default_config, 
-        baseline_var_pars, 
-        const_var_pars,
-        baseline_model,
-        "test_nl_all.json"
-        )
+    
+    # the actual datasets
+    datasets = DATASET_REFERENCE.keys()
+    
+    for dataset_name in datasets:
+        result_name = f"all_loss_nl_{dataset_name}.json"
+        default_config["dataset"] = dataset_name
+        run_bunch_experiments(
+            default_config, 
+            baseline_var_pars, 
+            const_var_pars,
+            baseline_model,
+            result_name
+            )
 
 
 if __name__ == "__main__":
